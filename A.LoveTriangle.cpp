77cd62bbb42eafeb2ec5a59412b6a378c09ede3e
samadasyed <samadasyed@hotmail.com>
2025-11-10 23:08:45 -0600

    Initial I/O

---
+++ A.LoveTriangle.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h> 
+
+using namespace std; 
+int main () {
+    int numOfTestCases; 
+    cin >> numOfTestCases; 
+    vector <int> sizes; 
+    vector <int> enemyPositions; 
+    vector<int>friendlypositions; 
+    for (int i = 0; i < numOfTestCases; i++){
+        cin>>sizes[i]>>enemyPositions[i]>>friendlypositions[i]; 
+    }
+
+
+    
+    return 0; 
+}
