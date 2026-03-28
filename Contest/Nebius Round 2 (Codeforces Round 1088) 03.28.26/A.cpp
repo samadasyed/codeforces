a9f946a51f060640c16882ffb6669f767f9b1be4
samadasyed <samadasyed@hotmail.com>
2026-03-28 10:15:14 -0500

    Changed some directories

---
+++ Contest/Nebius Round 2 (Codeforces Round 1088) 03.28.26/A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+typedef long long ll; 
+
+
+int main () {
+    cin.tie(0) -> sync_with_stdio(0);
+    cin.exceptions(cin.failbit); 
+    
+    int numOfArrays; 
+    cin >> numOfArrays; 
+    for (int i = 0; i < numOfArrays; i++) {
+        int n; 
+        cin >> n; 
+        vector<int> arr(n); 
+
+    for (int j = 0; j < n; j++){
+        cin >> arr[j]; 
+    }
+
+    }
+
+    return 0; 
+}
