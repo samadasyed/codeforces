b4231e3903d16492a3a3c6446d50a067f61d73b9
samadasyed <samadasyed@hotmail.com>
2026-03-28 11:01:33 -0500

    Got the io working but struggling with permutation and sub 3 logic

--- Contest/Nebius Round 2 (Codeforces Round 1088) 03.28.26/A.cpp
+++ Contest/Nebius Round 2 (Codeforces Round 1088) 03.28.26/A.cpp
@@ -7,7 +7,7 @@ typedef long long ll;
 int main () {
     cin.tie(0) -> sync_with_stdio(0);
     cin.exceptions(cin.failbit); 
-    
+    vector<vector<int>> arrays; 
     int numOfArrays; 
     cin >> numOfArrays; 
     for (int i = 0; i < numOfArrays; i++) {
@@ -18,8 +18,18 @@ int main () {
     for (int j = 0; j < n; j++){
         cin >> arr[j]; 
     }
-
+    arrays.push_back(arr);
     }
+    for (const vector<int>& arr : arrays) {
+        if (arr.size() < 3) {
+            for (int x : arr) {
+                cout << x << "\n"; 
+            }
+            continue;
+        }
+    }
+
+
 
     return 0; 
 }
