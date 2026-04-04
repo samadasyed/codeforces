dff4ecd968648317e130aae96564e837d67ce9cd
samadasyed <samadasyed@hotmail.com>
2026-04-04 09:33:31 -0500

    April Fools Day Contest

---
+++ Contest/Aprils Fool's Day Contest 04.01.26/A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+typedef long long ll; 
+
+
+int main () {
+    cin.tie(0) -> sync_with_stdio(0);
+    cin.exceptions(cin.failbit); 
+
+    cout << "H" << endl;
+    cout << "1" << endl; 
+    return 0; 
+}
---
+++ Contest/Aprils Fool's Day Contest 04.01.26/D.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+typedef long long ll; 
+
+
+int main () {
+    cin.tie(0) -> sync_with_stdio(0);
+    cin.exceptions(cin.failbit); 
+    int x; 
+    cin >> x; 
+    if (x == 1){ 
+        cout << "walk" << endl; 
+    }
+    else if (x == 2){ 
+        cout << "no" << endl; 
+    }
+    else if (x == 3){ 
+        cout << "no" << endl; 
+
+    }
+    else if (x == 4){ 
+        cout << "no" << endl; 
+    }
+    else if (x == 5){ 
+        cout << "yes" << endl; 
+    }
+    else if (x == 6){ 
+        cout << "yes" << endl; 
+    }
+    else if (x == 7){ 
+        cout << "sdrawkcab" << endl; 
+    }
+    else if (x == 8){ 
+        cout << "9" << endl; 
+    } 
+
+    return 0; 
+}
