57874061a34b3fbd24ad5c8b5e46edb0a06ea487
samadasyed <samadasyed@hotmail.com>
2026-04-13 11:18:07 -0500

    Final

--- Contest/Codeforces Round 1093 (Div. 2) 04.13.2026/A.cpp
+++ Contest/Codeforces Round 1093 (Div. 2) 04.13.2026/A.cpp
@@ -7,5 +7,14 @@ typedef long long ll;
 int main () {
     cin.tie(0) -> sync_with_stdio(0);
     cin.exceptions(cin.failbit); 
+
+    int testCases; 
+    cin >> testCases;
+
+    for (int i = 0; i < testCases; i++); 
+
+
+
+
     return 0; 
 }
